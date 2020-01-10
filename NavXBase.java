23a11521ef7058416e2971ed56a26c264edda127
TheMageKing <calumlikesapplepie@gmail.com>
2020-01-09 20:48:15 -0500

    Flesh out NavX class a bit more

--- NavXBase.java
+++ NavXBase.java
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot.witchcraft;
 
 import com.kauailabs.navx.frc.*;
@@ -5,57 +12,73 @@ import edu.wpi.first.wpilibj.SPI;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
-
 /**
- * This is a class that manages a NavX
- * In proper implementations, this will 
+ * This is a class that manages a NavX. Copy-paste it, whatever The methods are
+ * corrected to the robot orientation TODO: Have the methods return the correct
+ * axis for their name: ie, correct for NavX orientation
  */
-public class NavXBase extends SubsystemBase{
+public class NavXBase extends SubsystemBase {
     AHRS NavX;
+
     /**
-     * This constructs the navx: it can throw an exception if the NavX is not found
+     * This constructs the navx: it may throw an exception if the NavX is not found
      */
-    public NavXBase(){//PUT ARGUMENTS WHEN WE NEED THEM
+    public NavXBase() {// PUT ARGUMENTS WHEN WE NEED THEM
         try {
-            NavX = new AHRS(SPI.Port.kMXP);//TODO: Check port num
+            NavX = new AHRS(SPI.Port.kMXP);// TODO: Check port num
         } catch (RuntimeException ex) {
-            //TODO:  We need to set up a drivers system output system: perhaps Robot.java should catch it
-            //Or we could output to the driver system directly, as they do in their example
-            //DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+            // TODO: We need to set up a drivers system output system: perhaps Robot.java
+            // should catch this exception
+            // Or we could output to the driver system directly, as they do in their example
+            // DriverStation.reportError("Error instantiating navX MXP: " + ex.getMessage(),
+            // true);
         }
-        
+
     }
 
     @Override
     public void periodic() {
-        //runs every scheduler run
+        // runs every scheduler run
     }
 
     /**
-     * Gets the pitch of the robot
-     * (pitch is rotation around the horisontal axis perpendicular to straight forward)
+     * Gets the pitch of the robot (X axis rotation) (pitch is rotation around the
+     * horisontal axis perpendicular to straight forward)
      * 
      * @return The pitch of the robot
      */
-    public double getPitch(){
+    public double getPitch() {
         return NavX.getPitch();
     }
+
     /**
-     * Gets the roll of the robot
-     * (roll is the leaning around the axis that goes straight forward)
+     * Gets the roll of the robot (Y axis rotation) (roll is the leaning around the
+     * axis that goes straight forward)
+     * 
      * @return
      */
-    public double getRoll(){
+    public double getRoll() {
         return NavX.getRoll();
     }
+
     /**
-     * Gets the yaw of the robot
-     * (yaw is the direction that the robot is facing around an axis that shoots straight up)
+     * Gets the yaw of the robot (Z axis rotation) (yaw is the direction that the
+     * robot is facing around an axis that shoots straight up)
+     * 
      * @return
      */
-    public double getYaw(){
+    public double getYaw() {
         return NavX.getYaw();
     }
-    
+
+    /**
+     * Zeroes the yaw of the robot
+     * 
+     * @return The previous yaw
+     */
+    public double zeroYaw() {
+        double temporaryDouble = NavX.getYaw();
+        NavX.zeroYaw();
+        return temporaryDouble;
+    }
 }
